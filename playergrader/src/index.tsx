197965a5f1b31a27deecd4396dc57933c5dac899
Bill McCoy <bmc410@comcast.net>
2023-08-08 10:54:23 -0400

    npm

--- playergrader/src/index.tsx
+++ playergrader/src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css"; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
